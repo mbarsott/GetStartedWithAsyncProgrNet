32dd1b1c85c4da02c3a4269be010b37a0228b7af
Marcelo Barsotti <mbarsott@gmail.com>
2019-04-21 17:23:30 -0400

    Initial Commint with bare minimum changes that enable the application to run Squashed with Remove file above GitHub size limit

---
+++ StockAnalyzer/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using StockAnalyzer.Core.Domain;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace StockAnalyzer.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            #region Before loading stock data
+            var watch = new Stopwatch();
+            watch.Start();
+            StockProgress.Visibility = Visibility.Visible;
+            StockProgress.IsIndeterminate = true;
+            #endregion
+
+            var client = new WebClient();
+
+            var content = client.DownloadString($"http://localhost:61363/api/stocks/{Ticker.Text}");
+
+            var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+
+            Stocks.ItemsSource = data;
+
+            #region After stock data is loaded
+            StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
+            StockProgress.Visibility = Visibility.Hidden;
+            #endregion
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
