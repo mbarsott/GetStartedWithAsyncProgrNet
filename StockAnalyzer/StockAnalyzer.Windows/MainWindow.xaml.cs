a35706ef661c5dac525bcb7293b0920955e84366
Marcelo Barsotti <mbarsott@gmail.com>
2019-04-21 18:42:40 -0400

    2. Asynchronous Programming in .NET using async and await

--- StockAnalyzer/StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
 using StockAnalyzer.Core.Domain;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Navigation;
 
@@ -15,7 +17,7 @@ namespace StockAnalyzer.Windows
             InitializeComponent();
         }
 
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private async void Search_Click(object sender, RoutedEventArgs e)
         {
             #region Before loading stock data
             var watch = new Stopwatch();
@@ -24,18 +26,63 @@ namespace StockAnalyzer.Windows
             StockProgress.IsIndeterminate = true;
             #endregion
 
-            var client = new WebClient();
+            //            using (var client = new HttpClient())
+            //            {
+            //
+            //                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
+            //
+            //                try
+            //                {
+            //                    response.EnsureSuccessStatusCode();
+            //
+            //                    var content = await response.Content.ReadAsStringAsync();
+            //
+            //                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+            //
+            //                    Stocks.ItemsSource = data;
+            //                }
+            //                catch (Exception ex)
+            //                {
+            //                    Notes.Text += ex.Message;
+            //                }
+            //            }
 
-            var content = client.DownloadString($"http://localhost:61363/api/stocks/{Ticker.Text}");
-
-            var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
-
-            Stocks.ItemsSource = data;
-
+            try
+            {
+                await GetStocks();
+            }
+            catch (Exception ex)
+            {
+                Notes.Text += ex.Message;
+            }
             #region After stock data is loaded
             StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
             StockProgress.Visibility = Visibility.Hidden;
             #endregion
+        }
+
+        public async Task GetStocks()
+        {
+            using (var client = new HttpClient())
+            {
+
+                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
+
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+
+                    Stocks.ItemsSource = data;
+                }
+                catch (Exception ex)
+                {
+                    Notes.Text += ex.Message;
+                }
+            }
         }
 
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
